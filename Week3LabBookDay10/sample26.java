c1f140bc9c2733b7462f5342b1638f058f0a50cd
Ishtiaqahd <86680789+Ishtiaqahd@users.noreply.github.com>
2021-07-24 14:36:37 +0530

    Add files via upload

---
+++ Week3LabBookDay10/sample26.java
@@ -0,0 +1,22 @@
+package Week3LabBookDay10;
+/**
+ 26. What is the problem with the following code? It compiles and runs,
+     but the output is numbers are the same. However, the numbers are not
+     the same.
+     sample.java
+ Author:KGN 21106 - Ishtiaq Ahmed
+**/
+public class sample26 {
+ public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int i = -243;
+ int j = 243;
+ //  if(i == j); end of if statement
+ //   System.out.println("numbers are the same");
+
+ if(i == j)
+  System.out.println("numbers are the same");
+ else
+  System.out.println("numbers are not the same");
+ }
+}
